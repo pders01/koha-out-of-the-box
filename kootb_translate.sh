4627701a3a580abdbf075fa4cb2827c50e7c764a
pders01 <49939682+pders01@users.noreply.github.com>
2020-12-04 18:46:16 +0100

    fixed typo; still needs improvement for usability

--- kootb_translate.sh
+++ kootb_translate.sh
@@ -2,7 +2,7 @@
 
 koha-translate --list --available | less
 
-echo "Select the language code from the list above to install the coresponding locale for koha"
+echo "Select the language code from the list above to install the corresponding locale for koha"
 read language_code
 
 koha-translate --install ${language_code}
